2005dcdd423304b5442a5a2d68dfeaacf40815ed
Morpheus <mangoo.pay@gmail.com>
2023-10-07 19:18:56 -0400

    feat: add the header page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,144 @@
 const Header = () => {
     return (
-        <></>
+        <header id="header" class="header">
+            <div class="header-top bg-theme-color-2 sm-text-center p-0">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="widget no-border m-0">
+                                <ul class="list-inline font-13 sm-text-center mt-5">
+                                    <li>
+                                        <a class="text-white" href="#">FAQ</a>
+                                    </li>
+                                    <li class="text-white">|</li>
+                                    <li>
+                                        <a class="text-white" href="#">Help Desk</a>
+                                    </li>
+                                    <li class="text-white">|</li>
+                                    <li>
+                                        <a class="text-white" href="#">Login</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="widget no-border m-0 pull-right flip sm-pull-none sm-text-center">
+                                <ul class="styled-icons icon-circled icon-sm pull-right flip sm-pull-none sm-text-center mt-sm-15">
+                                    <li><a href="#"><i class="fa fa-facebook text-white"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter text-white"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-google-plus text-white"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-instagram text-white"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin text-white"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-middle p-0 bg-lightest xs-text-center">
+                <div class="container pt-0 pb-0">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-4 col-md-5">
+                            <div class="widget no-border m-0">
+                                <a class="menuzord-brand pull-left flip xs-pull-center" href="javascript:void(0)"><img src="assets/images/logo-wide.png" alt="" /></a>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 col-md-4">
+                            <div class="widget no-border pull-right sm-pull-none sm-text-center mt-10 mb-10 m-0">
+                                <ul class="list-inline">
+                                    <li><i class="fa fa-phone-square text-theme-colored font-36 mt-5 sm-display-block"></i></li>
+                                    <li>
+                                        <a href="#" class="font-12 text-gray text-uppercase">Call us today!</a>
+                                        <h5 class="font-14 m-0"> +(012) 345 6789</h5>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>  
+                        <div class="col-xs-12 col-sm-4 col-md-3">
+                            <div class="widget no-border pull-right sm-pull-none sm-text-center mt-10 mb-10 m-0">
+                                <ul class="list-inline">
+                                    <li><i class="fa fa-clock-o text-theme-colored font-36 mt-5 sm-display-block"></i></li>
+                                    <li>
+                                        <a href="#" class="font-12 text-gray text-uppercase">We are open!</a>
+                                        <h5 class="font-13 text-black m-0"> Mon-Fri 8:00-16:00</h5>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-nav">
+                <div class="header-nav-wrapper navbar-scrolltofixed bg-theme-colored border-bottom-theme-color-2-1px">
+                    <div class="container">
+                    <nav id="menuzord" class="menuzord bg-theme-colored pull-left flip menuzord-responsive">
+                        <ul class="menuzord-menu">
+                            <li class="active"><a href="#home">Home</a>
+                                <ul class="dropdown">
+                                    <li>
+                                        <a href="#">Multi Page Layouts</a>
+                                        <ul class="dropdown">
+                                            <li><a href="index-mp-layout1.html">Layout1</a></li>
+                                            <li><a href="index-mp-layout2.html">Layout2</a></li>
+                                            <li><a href="index-mp-layout3.html">Layout3</a></li>
+                                            <li><a href="index-mp-layout4.html">Layout4</a></li>
+                                            <li><a href="index-mp-layout5.html">Layout5</a></li>
+                                            <li><a href="index-mp-layout6.html">Layout6</a></li>
+                                            <li><a href="index-mp-layout7.html">Layout7 <span class="label label-info">New</span></a></li>
+                                            <li><a href="index-mp-layout8.html">Layout8 <span class="label label-info">New</span></a></li>
+                                            <li><a href="index-mp-layout9.html">Layout9 <span class="label label-info">New</span></a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="index-shop-home.html">Shop Home <span class="label label-info">New</span></a></li>
+                                    
+                                </ul>
+                            </li>
+                            <li><a href="#">Features <span class="label label-info">New</span></a>
+                                <ul class="dropdown">
+                                    <li><a href="features-preloader.html">Preloaders</a></li>
+                                    <li><a href="#">Header</a>
+                                        <ul class="dropdown">
+                                            <li><a href="features-header-style1.html">Header Style1</a></li>
+                                            <li><a href="features-header-style2.html">Header Style2</a></li>
+                                            <li><a href="features-header-style3.html">Header Style3</a></li>
+                                            <li><a href="features-header-style4.html">Header Style4</a></li>
+                                            <li><a href="features-header-style5.html">Header Style5</a></li>
+                                            <li><a href="features-header-style6.html">Header Style6</a></li>
+                                            <li><a href="features-header-style7.html">Header Style7</a></li>
+                                            <li><a href="features-header-vertical-nav.html">Vertical Header</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Page Title</a>
+                                        <ul class="dropdown">
+                                            <li><a href="features-page-title-text-left.html">Text Left</a></li>
+                                            <li><a href="features-page-title-text-center.html">Text Center</a></li>
+                                            <li><a href="features-page-title-text-right.html">Text Right</a></li>
+                                            <li><a href="features-page-title-mini-version.html">Mini Version</a></li>
+                                            <li><a href="features-page-title-big-version.html">Big Version</a></li>
+                                            <li><a href="features-page-title-extra-big-version.html">Extra Big Version</a></li>
+                                            <li><a href="features-page-title-bg-white.html">Bg White</a></li>
+                                            <li><a href="features-page-title-bg-image.html">Bg Image</a></li>
+                                            <li><a href="features-page-title-bg-image-parallax.html">Bg Image Parallax</a></li>
+                                            <li><a href="features-page-title-bg-video.html">Bg Video</a></li>
+                                            <li><a href="features-page-title-full-transparent.html">Full Transparent</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Side Push Panel</a>
+                                        <ul class="dropdown">
+                                            <li><a href="features-side-push-panel-left-overlay.html">Left Overlay</a></li>
+                                            <li><a href="features-side-push-panel-left-push.html">Left Push</a></li>
+                                            <li><a href="features-side-push-panel-right-overlay.html">Right Overlay</a></li>
+                                            <li><a href="features-side-push-panel-right-push.html">Right Push</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            </div>
+    </header>
     )
 }
 
