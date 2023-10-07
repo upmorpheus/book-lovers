f63fa96feae4241e9d7150c08c15d7500811cca1
Morpheus <mangoo.pay@gmail.com>
2023-10-07 10:54:20 -0400

    feat: add the router in App.js commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useSelector } from 'react-redux';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
 import Home from "./views";
+import Login from "./views/auth/Login";
 import './App.scss';
 
+const publicRouter = createBrowserRouter(createRoutesFromElements([<Route path='/' element={<Home />} />, <Route path='/login' element={<Login />} />]))
+
+const privateRouter = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Home />}>
+            <Route path='start'>
+                <Route path='home' element={<Home />} />
+            </Route>
+        </Route>,
+    ),
+);
+
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
-}
+    const logged = useSelector((state) => state.app.data)
+    return <RouterProvider router={!logged ? privateRouter : publicRouter} />
+};
 
 export default App;
