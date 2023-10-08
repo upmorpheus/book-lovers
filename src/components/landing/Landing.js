928b2e7f805b178b1d560841bc6b7b7e2d78b8f4
Morpheus <mangoo.pay@gmail.com>
2023-10-07 23:06:52 -0400

    feat: update the pattern side commit

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -12,7 +12,20 @@ const Landing = () => {
         <div className="landing">
             <Carouselfade slide1={slide1} slide2={slide2} slide3={slide3} />
             <div className="pattern">
-                <Image src={slide4} rounded/>
+                <Image className="image-left" src={slide4} rounded/>
+                <div className="pattern-text">
+                    <h1>THE WORLD’S BEST EDUCATION</h1>
+                    <p>Many experts throughout history have regarded cancer salves and pastes as the
+                         most thorough, safe, and efficacious way to treat cancer, especially skin and 
+                         breast cancers but also cancers of other organs. In this book, Ingrid Naiman 
+                         meticulously traces the use of such products in ancient India and by Hildegard of 
+                         Bingen, Native Americans, and modern physicians. She provides detailed instructions 
+                         for making and using the salves, a fair comparison of the pros and cons, and eight 
+                         pages of full color pictures showing responses to the products. Visit Cancer Salves 
+                         site for more information, answers to frequently asked questions, and a Checklist 
+                         for people facing cancer.
+                    </p>
+                </div>
             </div>
         </div>
     );
