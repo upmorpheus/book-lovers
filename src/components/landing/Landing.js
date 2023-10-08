c8376bbe38d62bad1fbf3c95c51bd02ef2824553
Morpheus <mangoo.pay@gmail.com>
2023-10-07 22:14:14 -0400

    feat: carousel fade complete

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Carouselfade from "../carousel/CarouselSlow";
+import slide1 from "../../assets/banner-3.png";
+import slide2 from "../../assets/banner-1.jpg";
+import slide3 from "../../assets/banner-2.jpg";
 
 const Landing = () => {
     return (
-        <>
-            
-        </>
+        <div className="landing">
+            <Carouselfade slide1={slide1} slide2={slide2} slide3={slide3} />
+        </div>
     );
 }
 
