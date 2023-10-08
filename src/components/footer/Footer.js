d4bfef141fae6259c18e064aef27f17183696178
Morpheus <mangoo.pay@gmail.com>
2023-10-08 11:23:05 -0400

    feat: add the icon bootstrap commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Icon } from './Icon';
 import './Footer.scss';
 
 const Footer = () => (
     <div className="footer">
         <div className="contact">
             <h2>Contact Me</h2>
-            <a>ahimsainternational@protonmail.com</a>
+            <a>
+                <Icon
+                    iconName="Stopwatch"
+                    color="royalblue"
+                    size={96}
+                    className="align-top"
+                />
+                ahimsainternational@protonmail.com
+            </a>
             <a>123 456 7890</a>
             <a></a>
             <a href="https://bioethikapress.com">www.bioethikapress.com</a>
