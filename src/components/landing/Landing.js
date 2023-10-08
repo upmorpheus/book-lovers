ef66e0df4f6fa312ad3c4f02b86524e80a39fe3b
Morpheus <mangoo.pay@gmail.com>
2023-10-08 06:42:30 -0400

    feat: update the color and text width

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,16 +1,28 @@
 import React from "react";
 import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import Carouselfade from "../carousel/CarouselSlow";
-import slide1 from "../../assets/banner-3.png";
-import slide2 from "../../assets/banner-1.png";
-import slide3 from "../../assets/banner-2.png";
+import slide1 from "../../assets/1.jpeg";
+import slide2 from "../../assets/2.jpeg";
+import slide3 from "../../assets/4.jpeg";
 import slide4 from "../../assets/4.png";
+import book1 from "../../assets/book1.png";
+import book2 from "../../assets/book2.png";
+import book3 from "../../assets/book3.png";
 import './Landing.scss';
+
+const Books = [
+    { name: "Immunity", src: book1, price: 35, views: 30 },
+    { name: "Parasite Cleansing", src: book2, price: 50, views: 30 },
+    { name: "Kitchen Doctor", src: book3, price: 65, views: 70 }
+];
 
 const Landing = () => {
     return (
         <div className="landing">
             <Carouselfade slide1={slide1} slide2={slide2} slide3={slide3} />
+
             <div className="pattern">
                 <Image className="image-left" src={slide4} rounded/>
                 <div className="pattern-text">
@@ -27,6 +39,27 @@ const Landing = () => {
                     </p>
                 </div>
             </div>
+
+            <div className="books">
+                {Books.map((item) =>
+                    <Card className="book-card">
+                        <Card.Img className="book-logo" variant="top" src={item.src} />
+                        <Card.Body>
+                            <Card.Text className="card-text-book">
+                                <a>{item.name}</a>                                
+                                <a>{item.views}</a>
+                                <a>${item.price}</a>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                )}
+            </div>
+
+            <div className="parellax">
+                <img src={bgimg} />
+            </div>
+
+            
         </div>
     );
 }
