4ffa1c8f1ec1cf01968d4e3771d30c1257c84897
upmorpheus <mangoo.pay@gmail.com>
2023-10-11 06:10:48 -0400

    feat: login page design background complete

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
-import SideBarImg from "../../assets/3.jpeg";
+import SideBarImg from "../../assets/login.png";
 import './Login.scss';
 
 const Login = () => {
@@ -13,23 +13,24 @@ const Login = () => {
                 <Card.Body>
                     <Image className="left-side" src={SideBarImg} rounded/>
                     <div className="right-side">
-                        <p><h3>Hello!</h3></p>
-                        <p><h2>Good morning</h2></p>
+                        <h5>Hello!</h5>
+                        <h4 className="mb-4">Good morning</h4>
                         <div className="center-side">
                             <Card.Title>Login Your Account</Card.Title>
-                            <Card.Text>
-                                <Form>
-                                    <Form.Group controlId="exampleForm.ControlInput1">
-                                        <Form.Label>Email address</Form.Label>
-                                        <Form.Control type="email" placeholder="name@example.com" />
-                                    </Form.Group>
-                                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                                        <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" placeholder="Please input your password." />
-                                    </Form.Group>
-                                </Form>
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
+                            <Form>
+                                <Form.Group controlId="exampleForm.ControlInput1">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="name@example.com" />
+                                </Form.Group>
+                                <Form.Group controlId="exampleForm.ControlTextarea1">
+                                    <Form.Label className="mt-4">Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Please input your password." />
+                                </Form.Group>
+                            </Form>
+                            <div className="btn-line">
+                                <Button className="mt-4" variant="primary">Go somewhere</Button>
+                                <Button className="mt-4" variant="success">Go somewhere</Button>
+                            </div>
                         </div>
                     </div>
                 </Card.Body>
