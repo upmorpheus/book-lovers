830a66a1404b3de6c6ddc9d614b7d200d5b2f45f
upmorpheus <mangoo.pay@gmail.com>
2023-10-11 04:46:36 -0400

    fix: login page scss

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 import SideBarImg from "../../assets/3.jpeg";
 import './Login.scss';
 
@@ -14,11 +15,22 @@ const Login = () => {
                     <div className="right-side">
                         <p><h3>Hello!</h3></p>
                         <p><h2>Good morning</h2></p>
-                        <Card.Title>Login Your Account</Card.Title>
-                        <Card.Text>
-                            With supporting text below as a natural lead-in to additional content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <div className="center-side">
+                            <Card.Title>Login Your Account</Card.Title>
+                            <Card.Text>
+                                <Form>
+                                    <Form.Group controlId="exampleForm.ControlInput1">
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control type="email" placeholder="name@example.com" />
+                                    </Form.Group>
+                                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" placeholder="Please input your password." />
+                                    </Form.Group>
+                                </Form>
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </div>
                     </div>
                 </Card.Body>
             </Card>
