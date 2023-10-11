bb71a9d2aa03be9510b2732805ed82fa1e559a4a
Hiroki Moto <hiroki.moto.pro@gmail.com>
2023-10-11 01:12:26 -0400

    feat: login page design feature

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import './Login.scss';
 
 const Login = () => {
     return (
         <div className="login">
-            
+            <Card>
+                <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                        With supporting text below as a natural lead-in to additional content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
