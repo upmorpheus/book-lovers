116ba89db9d6d5427c36e87f00170bc838d8d409
upmorpheus <mangoo.pay@gmail.com>
2023-10-24 06:52:46 -0400

    style: complete the project

--- src/views/Management.js
+++ src/views/Management.js
@@ -15,22 +15,26 @@ const Management = () => {
                     <div className="form-page">
                         <Form>
                             <Form.Group className="mb-3" controlId="formGroupEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Label>Book Name</Form.Label>
+                                <Form.Control type="text" placeholder="Please input your book name" />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formGroupPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Label>Pages</Form.Label>
+                                <Form.Control type="number" placeholder="Please input book's pages" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formGroupEmail">
+                                <Form.Label>Price</Form.Label>
+                                <Form.Control type="number" placeholder="Please input price" />
                             </Form.Group>
                         </Form>
                         
                         <Form>
                             <Form.Group className="mb-3" controlId="formGroupEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Label>Price</Form.Label>
+                                <Form.Control type="number" placeholder="Please input price" />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formGroupPassword">
-                                <Form.Label>Password</Form.Label>
+                                <Form.Label>Up</Form.Label>
                                 <Form.Control type="password" placeholder="Password" />
                             </Form.Group>
                         </Form>
